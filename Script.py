6521c2a0d6a9496b2a67f74be8faebff05d71707
rinrinx <91373907+rinrinx@users.noreply.github.com>
2022-04-22 16:35:05 +0300

    Update Script.py

--- Script.py
+++ Script.py
@@ -1,107 +1,88 @@
 class script(object):
-    START_TXT = """𝙷𝙴𝙻𝙾 {},
-𝙼𝚈 𝙽𝙰𝙼𝙴 𝙸𝚂 <a href=https://t.me/{}>{}</a>, 𝙸 𝙲𝙰𝙽 𝙿𝚁𝙾𝚅𝙸𝙳𝙴 𝙼𝙾𝚅𝙸𝙴𝚂, 𝙹𝚄𝚂𝚃 𝙰𝙳𝙳 𝙼𝙴 𝚃𝙾 𝚈𝙾𝚄𝚁 𝙶𝚁𝙾𝚄𝙿 𝙰𝙽𝙳 𝙴𝙽𝙹𝙾𝚈 😍"""
-    HELP_TXT = """𝙷𝙴𝚈 {}
-𝙷𝙴𝚁𝙴 𝙸𝚂 𝚃𝙷𝙴 𝙷𝙴𝙻𝙿 𝙵𝙾𝚁 𝙼𝚈 𝙲𝙾𝙼𝙼𝙰𝙽𝙳𝚂."""
-    ABOUT_TXT = """✯ 𝙼𝚈 𝙽𝙰𝙼𝙴: {}
-✯ 𝙲𝚁𝙴𝙰𝚃𝙾𝚁: <a href=https://t.me/TeamEvamaria>Team Eva Maria</a>
-✯ 𝙻𝙸𝙱𝚁𝙰𝚁𝚈: 𝙿𝚈𝚁𝙾𝙶𝚁𝙰𝙼
-✯ 𝙻𝙰𝙽𝙶𝚄𝙰𝙶𝙴: 𝙿𝚈𝚃𝙷𝙾𝙽 𝟹
-✯ 𝙳𝙰𝚃𝙰 𝙱𝙰𝚂𝙴: 𝙼𝙾𝙽𝙶𝙾 𝙳𝙱
-✯ 𝙱𝙾𝚃 𝚂𝙴𝚁𝚅𝙴𝚁: 𝙷𝙴𝚁𝙾𝙺𝚄
-✯ 𝙱𝚄𝙸𝙻𝙳 𝚂𝚃𝙰𝚃𝚄𝚂: v1.0.1 [ 𝙱𝙴𝚃𝙰 ]"""
+    START_TXT = """👋  Merhaba {}\n
+ 🔎 Ara Butonunu Kullanın.."""
+    HELP_TXT = """👋 Merhaba {}
+Komutlarım İçin Yardım ."""
+    ABOUT_TXT = """✯ Kutuphane Botu
+✯ Yaratıcı: kamileecher
+✯ Kütüphane: Pyrogram
+✯ Dil: Python 3
+✯ Veri Tabanı: db
+✯ Bot 𝚂unucusu: xxxx
+✯ Yapı Durumu: v1.0.1 [ 𝙱𝙴𝚃𝙰 ]"""
     SOURCE_TXT = """<b>NOTE:</b>
-- Eva Maria is a open source project. 
-- Source - https://github.com/EvamariaTG/EvaMaria  
-
-<b>DEVS:</b>
-- <a href=https://t.me/TeamEvamaria>Team Eva Maria</a>"""
-    MANUELFILTER_TXT = """Help: <b>Filters</b>
-
-- Filter is the feature were users can set automated replies for a particular keyword and EvaMaria will respond whenever a keyword is found the message
-
-<b>NOTE:</b>
-1. eva maria should have admin privillage.
-2. only admins can add filters in a chat.
-3. alert buttons have a limit of 64 characters.
-
-<b>Commands and Usage:</b>
-• /filter - <code>add a filter in chat</code>
-• /filters - <code>list all the filters of a chat</code>
-• /del - <code>delete a specific filter in chat</code>
-• /delall - <code>delete the whole filters in a chat (chat owner only)</code>"""
-    BUTTON_TXT = """Help: <b>Buttons</b>
-
-- Eva Maria Supports both url and alert inline buttons.
-
-<b>NOTE:</b>
-1. Telegram will not allows you to send buttons without any content, so content is mandatory.
-2. Eva Maria supports buttons with any telegram media type.
-3. Buttons should be properly parsed as markdown format
-
-<b>URL buttons:</b>
-<code>[Button Text](buttonurl:https://t.me/EvaMariaBot)</code>
-
-<b>Alert buttons:</b>
-<code>[Button Text](buttonalert:This is an alert message)</code>"""
-    AUTOFILTER_TXT = """Help: <b>Auto Filter</b>
-
-<b>NOTE:</b>
-1. Make me the admin of your channel if it's private.
-2. make sure that your channel does not contains camrips, porn and fake files.
-3. Forward the last message to me with quotes.
- I'll add all the files in that channel to my db."""
-    CONNECTION_TXT = """Help: <b>Connections</b>
-
-- Used to connect bot to PM for managing filters 
-- it helps to avoid spamming in groups.
-
-<b>NOTE:</b>
-1. Only admins can add a connection.
-2. Send <code>/connect</code> for connecting me to ur PM
-
-<b>Commands and Usage:</b>
-• /connect  - <code>connect a particular chat to your PM</code>
-• /disconnect  - <code>disconnect from a chat</code>
-• /connections - <code>list all your connections</code>"""
-    EXTRAMOD_TXT = """Help: <b>Extra Modules</b>
-
-<b>NOTE:</b>
-these are the extra features of Eva Maria
-
-<b>Commands and Usage:</b>
-• /id - <code>get id of a specified user.</code>
-• /info  - <code>get information about a user.</code>
-• /imdb  - <code>get the film information from IMDb source.</code>
-• /search  - <code>get the film information from various sources.</code>"""
-    ADMIN_TXT = """Help: <b>Admin mods</b>
-
-<b>NOTE:</b>
-This module only works for my admins
-
-<b>Commands and Usage:</b>
-• /logs - <code>to get the rescent errors</code>
-• /stats - <code>to get status of files in db.</code>
-• /delete - <code>to delete a specific file from db.</code>
-• /users - <code>to get list of my users and ids.</code>
-• /chats - <code>to get list of the my chats and ids </code>
-• /leave  - <code>to leave from a chat.</code>
-• /disable  -  <code>do disable a chat.</code>
-• /ban  - <code>to ban a user.</code>
-• /unban  - <code>to unban a user.</code>
-• /channel - <code>to get list of total connected channels</code>
-• /broadcast - <code>to broadcast a message to all users</code>"""
-    STATUS_TXT = """★ 𝚃𝙾𝚃𝙰𝙻 𝙵𝙸𝙻𝙴𝚂: <code>{}</code>
-★ 𝚃𝙾𝚃𝙰𝙻 𝚄𝚂𝙴𝚁𝚂: <code>{}</code>
-★ 𝚃𝙾𝚃𝙰𝙻 𝙲𝙷𝙰𝚃𝚂: <code>{}</code>
-★ 𝚄𝚂𝙴𝙳 𝚂𝚃𝙾𝚁𝙰𝙶𝙴: <code>{}</code> 𝙼𝚒𝙱
-★ 𝙵𝚁𝙴𝙴 𝚂𝚃𝙾𝚁𝙰𝙶𝙴: <code>{}</code> 𝙼𝚒𝙱"""
+- Yonetici - https://t.me/kamileecher 
+<b>BILGI:</b>
+- <a href=https://t.me/sanaldisk>Livebox</a>"""
+    MANUELFILTER_TXT = """Yardım: <b>Filters</b>
+<b>NOT:</b>
+1. Kutuphane yönetici  olmalı.
+2. Bir sohbete yalnızca yöneticiler filtre ekleyebilir.
+3. Uyarı düğmelerinin sınırı 64 karakterdir. 
+<b>Komutlar Ve Kullanım:</b>
+• /filter - <code>Sohbet için bir filtre ekle</code>
+• /filters - <code>Tüm Filtrelerin listesi</code>
+• /del - <code>sohbette belirli bir filtreyi silme</code>
+• /delall - <code>sohbetteki tüm filtreleri silme (Sohbet Kurucusu Sadece)</code>"""
+    BUTTON_TXT = """Yardım: <b>Butonlar</b>
+- Kutuphane Botu Hem URL hem de uyarı satır içi düğmelerini destekler .
+<b>NOT:</b>
+1. Telegram herhangi bir içerik olmadan düğme göndermenize izin vermez, bu nedenle içerik zorunludur.
+2. Quickwaste Film Botu, herhangi bir telegram medya türüne sahip düğmeleri destekler.
+3. Düğmeler markdown biçimi olarak düzgün bir şekilde ayrıştırılmalıdır
+<b>URL Butonları:</b>
+<code>[Button Text](buttonurl:https://t.me/kamileecher)</code>
+<b>Uyarı Butonları:</b>
+<code>[Button Text](buttonalert:Uyarı Butonu)</code>"""
+    AUTOFILTER_TXT = """Yardım: <b>Auto Filter</b>
+<b>NOT:</b>
+1. Özelse beni kanalınızın yöneticisi yap.
+2. Kanalınızın kam rip, porno ve sahte dosyalar içermediğinden emin olun.
+3. Son mesajı bana alıntılarla iletin.
+ O kanaldaki tüm dosyaları veritabanıma ekleyeceğim. ."""
+    CONNECTION_TXT = """Help: <b>Baglantılar</b>
+- Filtreleri yönetmek için botu PM'ye bağlamak için kullanılır 
+- gruplar halinde spam'leri önlemeye yardımcı olur. 
+<b>NOT:</b>
+1. Yalnızca yöneticiler bağlantı ekleyebilir .
+2. Ozelden <code>/connect</code> komutunu gönder
+<b>Komutlar Ve Kullanım:</b>
+• /connect  - <code>belirli bir sohbeti özel sohbetinize bağlama</code>
+• /disconnect  - <code>sohbetten Bağlantıyı Kopartma</code>
+• /connections - <code>Tüm Bağlantılarının Listesi</code>"""
+    EXTRAMOD_TXT = """Help: <b>Extra Ozellik</b>
+<b>NOT:</b>
+Ekstra özellikler
+<b>Komutlar ve Kullanımı:</b>
+• /id - <code>Belirli Bir kullanıcının İd sini getirir.</code>
+• /info  - <code>Kulanıcıların Bilgisini Getirir.</code>
+• /imdb  - <code>İmdb Kaynağından Film Bilgisi getirir.</code>
+• /search  - <code>film bilgilerini çeşitli kaynaklardan almak.</code>"""
+    ADMIN_TXT = """Yardım: <b>Admin modu</b>
+<b>NOT:</b>
+Bu modül yalnızca yöneticim için çalışır. 
+<b>Komutlar ve Kullanımı:</b>
+• /logs - <code>rescent hatalarını almak için</code>
+• /stats - <code>DB'deki dosyaların durumunu almak için.</code>
+• /users - <code>kullanıcılarımın ve kimliklerimin listesini almak için.</code>
+• /chats - <code>sohbetlerimin ve kimliklerimin listesini almak için</code>
+• /leave  - <code>sohbetten ayrılmak için.</code>
+• /disable  -  <code>sohbeti devre dışı bırakma.</code>
+• /ban  - <code>kullanıcıyı yasaklamak için.</code>
+• /unban  - <code>Kullanıcının Banını Açma.</code>
+• /channnel - <code>toplam bağlı kanalların listesini almak için</code>
+• /broadcast - <code>Tüm Kutuphane kullanıcılarına mesaj yayınlamak için</code>"""
+    STATUS_TXT = """★ Toplam Dosya: <code>{}</code>
+★ Toplam Kullanıcı: <code>{}</code>
+★ 𝚃oplam Sohbet: <code>{}</code>
+★ Kullanılan Alan: <code>{}</code> 𝙼𝚒𝙱
+★ Boş Alan: <code>{}</code> 𝙼𝚒𝙱"""
     LOG_TEXT_G = """#NewGroup
-Group = {}(<code>{}</code>)
-Total Members = <code>{}</code>
-Added By - {}
+Grup = {}(<code>{}</code>)
+Toplam Üyeler = <code>{}</code>
+Eklendi tarafından - {}
 """
     LOG_TEXT_P = """#NewUser
 ID - <code>{}</code>
-Name - {}
+ADI - {}
 """
